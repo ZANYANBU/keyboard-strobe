885b28b315d68b9c5101c8e7fc6dd137ebe89967
Anbu <165456168+ZANYANBU@users.noreply.github.com>
2026-07-09 05:03:08 +0530

    Further increase strobe pulse width and spacing for maximum ON/OFF contrast visibility

--- Sources/KeyboardStrobe/main.swift
+++ Sources/KeyboardStrobe/main.swift
@@ -37,8 +37,8 @@ class AudioProcessor: NSObject, SCStreamOutput {
     var lastDetectedTime = Date()
     
     // Timing parameters to handle MacBook hardware LED latency
-    let holdOnDuration: TimeInterval = 0.09
-    let refractoryPeriod: TimeInterval = 0.20
+    let holdOnDuration: TimeInterval = 0.15
+    let refractoryPeriod: TimeInterval = 0.35
     
     // Delay queue for perfect audio sync calibration
     struct BeatEvent {
